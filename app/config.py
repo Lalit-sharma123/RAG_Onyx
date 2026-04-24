3b0021237eb6e239ee0d4d0bc91ddf3558dab454
Lalit Kumar Sharma <sharmaalalit555@gmail.com>
2026-04-24 16:51:11 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
-#OLLAMA_URL = "http://111.118.189.124:11435/api/generate"
-OLLAMA_URL = "http://111.118.189.124:11435"
+
+OLLAMA_URL = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
 MODEL = "gpt-oss:120b"
 
 EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
